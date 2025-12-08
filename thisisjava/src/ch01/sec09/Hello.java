a2e5ba6de668ff36b9d0a7cabc3439cd7282ab65
minho60 <minho031@gmail.com>
2025-12-08 14:34:35 +0900

    교재소스

---
+++ thisisjava/src/ch01/sec09/Hello.java
@@ -0,0 +1,16 @@
+package ch01.sec09;
+
+/** 
+ *  문서주석
+ */
+public class Hello {
+	/*
+	여러 줄 주석
+	여러줄
+	*/
+	public static void main(String[] args) {
+		// ctrl + /: 주석
+		// document.write("hello, Java");
+		System.out.println("Hello, Java");
+	}
+}
