a1628ac6ba4f0bea092b012a5064130674e7b540
minho60 <minho031@gmail.com>
2026-02-05 16:57:31 +0900

    0205

---
+++ jsp/my-jsp/src/main/java/controller/Hello.java
@@ -0,0 +1,10 @@
+package controller;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		System.out.println("Hello JSP");
+
+	}
+
+}
---
+++ jsp/my-jsp/src/main/java/controller/HelloServlet.java
@@ -0,0 +1,32 @@
+package controller;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+
+/*
+ * web.xml 또는 @WebServlet("URL 맵핑 주소") 애노테이션
+ * http://localhost:8080/프로젝트명/URL맵핑
+ * http://localhost:8080/my-jsp/hello  
+ * 
+ */
+
+@WebServlet("/hello")
+public class HelloServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("Hello JSP");
+		response.getWriter().println("Hello JSP");
+	}
+
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
